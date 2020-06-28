154d3209dd7b7edae669bf817c4174386beb8d82
Sefat Anam <sefatanam@gmail.com>
2020-06-28 15:25:51 +0600

    Project Initialie

---
+++ LikedList/DS-ALGO-LinkedList.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.val = value
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def add(self, value):
+        node = Node(value)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+
+        else:
+            self.head.next = node
+            node.prev = self.head
+            self.tail = node
+            self.size += 1
